627e064176db19c40af175f5581536faffa336db
pvkhai004 <pvkhaicd1998@gmail.com>
2019-09-23 08:39:40 +0700

    QR Scan

--- SAMNGOCLINH/app/src/main/java/com/pvkhaicd/samngoclinh/ViewController/QRScanActivity.java
+++ SAMNGOCLINH/app/src/main/java/com/pvkhaicd/samngoclinh/ViewController/QRScanActivity.java
@@ -1,30 +1,109 @@
 package com.pvkhaicd.samngoclinh.ViewController;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.zxing.Result;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.pvkhaicd.samngoclinh.R;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import java.io.IOException;
 
-public class QRScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView mScannerView;
+
+public class QRScanActivity extends AppCompatActivity {
+    SurfaceView cameraView;
+    EditText barcodeInfo;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
+        setContentView(R.layout.activity_qrscan);
 
+        cameraView =  findViewById(R.id.camera_view);
+        barcodeInfo = findViewById(R.id.code_info);
+        toolbar = findViewById(R.id.toolbar);
+        configToolbar();
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                .build();
+//
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                            barcodeInfo.setTextColor(getResources().getColor(R.color.black));
+                        }
+                    });
+                }
+            }
+        });
     }
-
 
     @Override
-    public void handleResult(Result rawResult) {
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+    private void configToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
+
 }
